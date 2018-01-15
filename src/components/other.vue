80e00d35a95adeb0e8ca02557099642247cce20a
Nicola Cordioli <cordioli.nicola@gmail.com>
2018-01-15 13:03:13 +0100

    add experiences and other

--- src/components/other.vue
+++ src/components/other.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1 class="pagetitle">Other</h1>
+    <div>
+        <h1 class="pagetitle">Other</h1>
+        <h2>Maybe you're here for some other reasons</h2>
+        <ul class="list">
+            <li><a target="_blank" href="https://www.facebook.com/ilTempodiunaStagione/">Il Tempo di una Stagione</a> - Italian indipendent full-length movie</li>
+            <li>Share - Short movie</li>
+            <li><a target="_blank" href="https://www.facebook.com/teatrinvisibili/">TeatrInvisibili</a> - Theater company</li>
+            <li><a target="_blank" href="https://www.facebook.com/AltriPostiinPiedi/">APP Altri Posti in Piedi</a> - Theater company</li>
+        </ul>
+
+    </div>
 </template>
--- src/components/workplaces.vue
+++ src/components/workplaces.vue
@@ -1,12 +1,56 @@
 <template>
   <div>
-      <h1 class="pagetitle">Education and workplaces</h1>
-      <h3>
-        Jobs
-      </h3>
-      <p>
-        I am currently working at E-Time srl, in Verona, from June 2015 as a Senior Fullstack Software developer. <br>
-        
-      </p>
+      <h1 class="pagetitle">Experience</h1>
+      <ul class="list">
+        <li>
+          <h3>
+            E-Time srl - from June 2015
+          </h3>
+          <p>
+            Senior Fullstack Software developer<br><br>
+            Working on corporate banking projects
+          </p>
+          <ul class="mansion-list">
+            <li>Giving estimate about time to develop changes requests</li>
+            <li>Helping organize the team</li>
+          </ul>
+        </li>
+        <li>
+          <h3>
+            Zucchetti SpA - from January 2011 to June 2015
+          </h3>
+          <p>
+            Senior Fullstack Software developer<br><br>
+            Working on Safety: Health and Safety Executive software
+          </p>
+          <ul class="mansion-list">
+            <li>Responsible of contract module</li>
+            <li>Giving estimate about time to develop changes requests</li>
+            <li>Helping organize the team</li>
+          </ul>
+        </li>
+        <li>
+          <h3>
+            Infogest srl - from January 2008 to June 2011
+          </h3>
+          <p>
+            Junior Fullstack Software developer<br><br>
+            Working on Campus3: Management software for gyms, pools, thermal baths and wellness centres
+          </p>
+          <ul class="mansion-list">
+            <li>Giving estimate about time to develop changes requests</li>
+          </ul>
+        </li>
+      </ul>
   </div>
 </template>
+
+<style>
+.mansion-list > li {
+  display: list-item;
+  list-style-type: circle;
+  margin-left: 16px;
+  padding: 0px;
+}
+</style>
+
